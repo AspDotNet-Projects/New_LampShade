0c13a47c8623fb019996a089ddbf04f0eba99346
AspDotNet-Projects <Pazdar_ali@yahoo.com>
2022-02-16 14:39:31 +0330

    IRepository in Framework

---
+++ LampShade/0_Framework/Domain/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace _0_Framework.Domain
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="Tkey"></typeparam> is id 
+    /// <typeparam name="T"></typeparam> is Table or property
+    public interface IRepository<Tkey,T>
+    {
+        T Get(Tkey id);
+        List<T> Get();
+
+        void Create(T entity);
+
+        bool Exists(Expression<Func<T, bool>> expression);
+        void SaveChange();
+    }
+}
