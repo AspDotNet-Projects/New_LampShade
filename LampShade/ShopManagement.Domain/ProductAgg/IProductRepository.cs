1b2e184e5c10e4a2f9c51ab5fdf059d54013103e
AspDotNet-Projects <Pazdar_ali@yahoo.com>
2022-02-21 13:25:41 +0330

    3)Add Custom Fun to IproductReposirory
    EditProduct GetDetails(long id);
            List<ProductSearchModel> search(ProductSearchModel searchModel);

--- LampShade/ShopManagement.Domain/ProductAgg/IProductRepository.cs
+++ LampShade/ShopManagement.Domain/ProductAgg/IProductRepository.cs
@@ -1,8 +1,12 @@
-﻿using _0_Framework.Domain;
+﻿using System.Collections.Generic;
+using _0_Framework.Domain;
+using ShopManagement.Application.Contract.Product;
 
 namespace ShopManagement.Domain.ProductAgg
 {
-    public interface IProductRepository:IRepository<long,Product> 
+    public interface IProductRepository:IRepository<long,Product>
     {
+        EditProduct GetDetails(long id);
+        List<ProductSearchModel> search(ProductSearchModel searchModel);
     }
 }
