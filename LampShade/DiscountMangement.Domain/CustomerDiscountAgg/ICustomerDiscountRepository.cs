2bbb9aee287ab5f4f1458cf1a495635e38d86649
alipazdar.projects@gmail.com <alipazdar.projects@gmail.com>
2022-03-12 21:12:16 +0330

    مرحله دوم پیاده سازی دامین برای مشتریان

---
+++ LampShade/DiscountMangement.Domain/CustomerDiscountAgg/ICustomerDiscountRepository.cs
@@ -0,0 +1,9 @@
+﻿using _0_Framework.Domain;
+
+namespace DiscountMangement.Domain.CustomerDiscountAgg
+{
+    public interface ICustomerDiscountRepository : IRepository<long,CustomerDiscount>
+    {
+
+    }
+}
