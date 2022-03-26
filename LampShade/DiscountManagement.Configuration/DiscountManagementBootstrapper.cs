5528301f495b07b90cda104acc0da27629e4d4d1
alipazdar.projects@gmail.com <alipazdar.projects@gmail.com>
2022-03-26 10:27:54 +0430

    init DiscountManagementConfiguration

---
+++ LampShade/DiscountManagement.Configuration/DiscountManagementBootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using DiscountManagement.Application;
+using DiscountManagement.Application.Contract.CustomerDiscount;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using DiscountManagement.Infrastructure.EFCore;
+using DiscountMangement.Domain.CustomerDiscountAgg;
+using DiscountManagement.Infrastructure.EFCore.Repository;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiscountManagement.Configuration
+{
+    public class DiscountManagementBootstrapper
+    {
+        public static void Configure(IServiceCollection services, string Connectionstring)
+        {
+            services.AddTransient<ICustomerDiscountApplication, CustomerDiscountApplication>();
+            services.AddTransient<ICustomerDiscountRepository, CustomerDiscountRepository>();
+
+            services.AddDbContext<DiscountContext>(x => x.UseSqlServer(Connectionstring));
+
+
+        }
+    }
+}
