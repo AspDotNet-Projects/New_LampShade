2c88b816770509d73d102c74fec85ffa138177bb
alipazdar.projects@gmail.com <alipazdar.projects@gmail.com>
2022-03-27 14:37:09 +0430

    مرحله سوم مقدار دهی ICollegueDiscountRepository با مقادیر خاص که که در ریپوزیتوری پیش فرض نیست

--- LampShade/DiscountMangement.Domain/ColleagueDiscountAgg/ICollegueDiscountRepository.cs
+++ LampShade/DiscountMangement.Domain/ColleagueDiscountAgg/ICollegueDiscountRepository.cs
@@ -1,8 +1,11 @@
 ﻿using _0_Framework.Domain;
+using DiscountManagement.Application.Contract.ColleagueDiscount;
 
 namespace DiscountMangement.Domain.ColleagueDiscountAgg
 {
     public interface ICollegueDiscountRepository:IRepository<long,ColleagueDiscount>
     {
+        EditColleagueDiscount GetDetails(long id);
+        ColleagueDiscountSearchModel Search(ColleagueDiscountSearchModel searchModel);
     }
 }
