3df781719c557688fdaf8117ecec25f1fb9d8bf6
alipazdar.projects@gmail.com <alipazdar.projects@gmail.com>
2022-03-30 17:10:29 +0430

    به علت پیچیدگی زیاد ابتدا لایه EfCore پیاده سازی شد قسمت مپینگ و اینونتوری کانتکس

---
+++ LampShade/InventoryManagement.Infrastructure.EFCore/Mapping/InventoryMapping.cs
@@ -0,0 +1,29 @@
+﻿using InventoryManagement.Domain.InventoryAgg;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace InventoryManagement.Infrastructure.EFCore.Mapping
+{
+    public class InventoryMapping : IEntityTypeConfiguration<Inventory>
+    {
+        public void Configure(EntityTypeBuilder<Inventory> builder)
+        {
+            builder.ToTable("Inventory");
+            builder.HasKey(x => x.Id);
+
+
+            ////OwnsMany با Hasmany
+            /// فرق میکنه که هز منی برای دو جدول جدا هستند
+            /// و آنها را اینکلود می کنیم
+            /// ولی واز منی دو جدول مرتبط هستند که نیاز به اینکلود نیست
+            builder.OwnsMany(x => x.Operations, ModelBuilder =>
+            {
+                ModelBuilder.HasKey(x => x.Id);
+                ModelBuilder.ToTable("Operations");
+                ModelBuilder.Property(x => x.Description).HasMaxLength(1000);
+                ModelBuilder.WithOwner(x => x.Inventory)
+                    .HasForeignKey(x => x.InventoryId);
+            });
+        }
+    }
+}
