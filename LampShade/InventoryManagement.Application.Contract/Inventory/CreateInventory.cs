d40c23a0fc8b38d8b68d0217b54a86b45ab64161
alipazdar.projects@gmail.com <alipazdar.projects@gmail.com>
2022-04-01 11:15:13 +0430

    ثبت ولیدیشن های مربوط به انبارداری

--- LampShade/InventoryManagement.Application.Contract/Inventory/CreateInventory.cs
+++ LampShade/InventoryManagement.Application.Contract/Inventory/CreateInventory.cs
@@ -1,11 +1,14 @@
 ﻿using ShopManagement.Application.Contract.Product;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using _0_Framework.Application;
 
 namespace InventoryManagement.Application.Contract.Inventory
 {
     public class CreateInventory
-    {
+    {   [Range(1,1000000,ErrorMessage = ValidationMesseges.IsRequired)]
         public long ProductId { get; set; }
+        [Range(1, double.MaxValue, ErrorMessage = ValidationMesseges.IsRequired)]
         public double UnitePrice { get; set; }
         public List<ProductViewModel> Products { get; set; }
     }
