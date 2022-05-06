85d5fa1092528562356ca8fe53c20424be29548a
alipazdar.projects@gmail.com <alipazdar.projects@gmail.com>
2022-05-06 11:24:44 +0430

    2) پیاده سازی اپلیکیشن کانترکت

---
+++ LampShade/BlogManagement.Application.Contracts/Article/IArticleApplication.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using _0_Framework.Application;
+
+namespace BlogManagement.Application.Contracts.Article
+{
+    public interface IArticleApplication
+    {
+        OperationResult Creaate(CreateArticle commnak);
+        OperationResult Edit(EditArticle command);
+        EditArticle Getdetails(long id);
+        List<ArticleViewModel> Search(ArticleSearchModel searchModel);
+    }
+}
