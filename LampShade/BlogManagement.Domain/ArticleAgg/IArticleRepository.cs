9a6f42040584736b3a492b1062e7bfad05dea4e5
alipazdar.projects@gmail.com <alipazdar.projects@gmail.com>
2022-05-06 11:28:09 +0430

    مربوط به قسمت یک پیاده سازی IArticleRepository

---
+++ LampShade/BlogManagement.Domain/ArticleAgg/IArticleRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _0_Framework.Domain;
+using BlogManagement.Application.Contracts.Article;
+
+namespace BlogManagement.Domain.ArticleAgg
+{
+    public interface IArticleRepository:IRepository<long,Article>
+    {
+        EditArticle Getdetails(long id);
+        List<ArticleViewModel> Search(ArticleSearchModel searchModel);
+    }
+}
