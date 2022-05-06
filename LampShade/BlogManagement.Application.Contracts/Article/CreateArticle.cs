0986c2999b950458fa8823452c53242239380114
alipazdar.projects@gmail.com <alipazdar.projects@gmail.com>
2022-05-06 16:10:12 +0430

    7)create
    قسمت
    <div class="modal-header">
        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
        <h4 class="modal-title">ایجاد گروه مقاله جدید</h4>
    </div>
    حذف و به جای آن قسمت سرچ میاریم
    و محتویات
    panel-body
    که در سرچ هست کاملا حذف
    و محتویات فرم اصلی جایگرین می شود

--- LampShade/BlogManagement.Application.Contracts/Article/CreateArticle.cs
+++ LampShade/BlogManagement.Application.Contracts/Article/CreateArticle.cs
@@ -1,20 +1,38 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using _0_Framework.Application;
+using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
 
 namespace BlogManagement.Application.Contracts.Article
 {
     public class CreateArticle
     {
+        [Required(ErrorMessage = ValidationMesseges.IsRequired)]
         public string Title { get;  set; }
+
+        [Required(ErrorMessage = ValidationMesseges.IsRequired)]
         public string ShortDescription { get;  set; }
         public string Description { get;  set; }
         public IFormFile Picture { get;  set; }
+        
+        [Required(ErrorMessage = ValidationMesseges.IsRequired)]
         public string PictureAlt { get;  set; }
+        
+        [Required(ErrorMessage = ValidationMesseges.IsRequired)]
         public string PictureTitle { get;  set; }
+        
+        [Required(ErrorMessage = ValidationMesseges.IsRequired)]
         public string PublishDate { get;  set; }
+        
+        [Required(ErrorMessage = ValidationMesseges.IsRequired)]
         public string Slug { get;  set; }
+        
+        [Required(ErrorMessage = ValidationMesseges.IsRequired)]
         public string Keywords { get;  set; }
+        
+        [Required(ErrorMessage = ValidationMesseges.IsRequired)]
         public string MetaDescription { get;  set; }
         public string CanonicalAddress { get;  set; }
+        [Range(1,long.MaxValue,ErrorMessage = ValidationMesseges.IsRequired)]
         public long CategoryId { get;  set; }
     }
 }
