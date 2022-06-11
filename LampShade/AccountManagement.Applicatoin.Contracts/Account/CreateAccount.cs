e89c918ec87143d7471c14d2e7f953971ce975d0
alipazdar.projects@gmail.com <alipazdar.projects@gmail.com>
2022-06-11 20:58:27 +0430

    تکمیل یو آی و ولیدیشن های مربوط به CreateAccount

--- LampShade/AccountManagement.Applicatoin.Contracts/Account/CreateAccount.cs
+++ LampShade/AccountManagement.Applicatoin.Contracts/Account/CreateAccount.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using _0_Framework.Application;
 using AccountManagement.Applicatoin.Contracts.Role;
 using Microsoft.AspNetCore.Http;
 
@@ -6,11 +8,21 @@ namespace AccountManagement.Applicatoin.Contracts.Account
 {
     public class CreateAccount
     {
+        [Required(ErrorMessage = ValidationMesseges.IsRequired)]
         public string Fullname { get;  set; }
+
+        [Required(ErrorMessage = ValidationMesseges.IsRequired)]
         public string Username { get;  set; }
+
+        [Required(ErrorMessage = ValidationMesseges.IsRequired)]
         public string Password { get;  set; }
+
+        [Required(ErrorMessage = ValidationMesseges.IsRequired)]
         public string Mobile { get;  set; }
+
+        [Range(1,int.MaxValue,ErrorMessage = ValidationMesseges.IsRequired)]
         public long RoleId { get;  set; }
+
         public List<RoleViewModel> Roles { get; set; }
         public IFormFile ProfilePhoto { get;  set; }
     }
