92c4d6e1d16f9cb50037920f23988e1adca4b24f
alipazdar.projects@gmail.com <alipazdar.projects@gmail.com>
2022-08-11 16:25:22 +0430

    پیاده سازی سبد خرید
    -آپدیت پیاده سازی تعدادمحصولات و خود محصولات
    1)جهت آپدیت تعدادکالا انتخاب شده در سبد خرید ابتدا باید فانکشنی نوشت تا مقدار را به صورت خودکار آپدیت کند
    
     function updateCart() {
            let products = $.cookie(cookieName);
            //length tedad item ha ro dare
            products = JSON.parse(products);
            $("#cart_items_count").text(products.length);
            const cartItemsWrapper = $("#cart_items_wrapper");
            //cod haye html dakhelesh ro khali kon
            cartItemsWrapper.html('');
            //be ezzaye har product in code haye html ro ezafe kon
            products.forEach(x => {
                const product = `<div class="single-cart-item">
                                <a href="javascript:void(0)" class="remove-icon" onclick="removeFromCart('${x.id}')">
                                    <i class="ion-android-close"></i>
                                </a>
                                <div class="image">
                                    <a href="single-product.html">
                                        <img src="/ProductPictures/${x.picture}"
                                             class="img-fluid" alt="">
                                    </a>
                                </div>
                                <div class="content">
                                    <p class="product-title">
                                        <a href="single-product.html">محصول: ${x.name}</a>
                                    </p>
                                    <p class="count">تعداد: ${x.count}</p>
                                    <p class="count">قیمت واحد: ${x.unitPrice}</p>
                                </div>
                            </div>`;
                //ezaje kardan Item be in tag
                cartItemsWrapper.append(product);
            });
        }
    که cart_items_wrapper داخل پیج منو   می باشد.
    
    <div class="mini-cart" id="mini-cart">
                                            <div class="cart-items-wrapper ps-scroll" id="cart_items_wrapper">
    
                                            </div>
                                            <div class="cart-buttons">
                                                <a asp-page="/Cart">مشاهده سبد خرید</a>
    </div>
    
      2)که cart_items_count در آن آی دی تگ در داخل پیج منو می باشد.
    
    <a href="#" id="minicart-trigger">
                                            <i class="ion-bag"></i>
                                            <span class="counter" id="cart_items_count">0</span>
    </a>
    3)قبل از آن باید بگیم که این تابع یعنی updateCart()  در لود پیج لیوت اجرا شود که به این صورت گفته می شود
    
    script>
                        //zaman lod shodan page products in fun load mishe
                        $(document).ready(function() {
                            updateCart();
                        })
    </script>
    -----حذف کردن از سبد خرید
    function removeFromCart(id) {
        let products = $.cookie(cookieName);
        //length tedad item ha ro dare
        products = JSON.parse(products);
        //find kardan oni ke id barabar ba paraneter vordi
        const itemToRemove = products.findIndex(x => x.id === id);
        //mige az on be bad ro chanta hazf konam
        products.splice(itemToRemove, 1);
        //save dar cookie var zaman expire va masir ke ba path da masir root yane "/" ast
        //dar baghe Update cookie ast
        $.cookie(cookieName, JSON.stringify(products), { expires: 2, path: "/" });
        updateCart();
    
    }
    که همانطورکه مشاهده می کنید این تابع در فانکشن updateCart(); صدا زده شده است و رویداد آن کلیک تگ آ می باشد
    <a href="javascript:void(0)" class="remove-icon" onclick="removeFromCart('${x.id}')">

--- LampShade/ServiceHost/wwwroot/Theme/assets/js/custom.js
+++ LampShade/ServiceHost/wwwroot/Theme/assets/js/custom.js
@@ -37,5 +37,54 @@ function addToCart(id, name, price, picture) {
     //save dar cookie var zaman expire va masir ke ba path da masir root yane "/" ast
     //dar baghe Update cookie ast
     $.cookie(cookieName, JSON.stringify(products), { expires: 2, path: "/" });
-  
+    updateCart();
+}
+
+    function updateCart() {
+        let products = $.cookie(cookieName);
+        //length tedad item ha ro dare
+        products = JSON.parse(products);
+        $("#cart_items_count").text(products.length);
+        const cartItemsWrapper = $("#cart_items_wrapper");
+        //cod haye html dakhelesh ro khali kon
+        cartItemsWrapper.html('');
+        //be ezzaye har product in code haye html ro ezafe kon
+        products.forEach(x => {
+            const product = `<div class="single-cart-item">
+                            <a href="javascript:void(0)" class="remove-icon" onclick="removeFromCart('${x.id}')">
+                                <i class="ion-android-close"></i>
+                            </a>
+                            <div class="image">
+                                <a href="single-product.html">
+                                    <img src="/ProductPictures/${x.picture}"
+                                         class="img-fluid" alt="">
+                                </a>
+                            </div>
+                            <div class="content">
+                                <p class="product-title">
+                                    <a href="single-product.html">محصول: ${x.name}</a>
+                                </p>
+                                <p class="count">تعداد: ${x.count}</p>
+                                <p class="count">قیمت واحد: ${x.unitPrice}</p>
+                            </div>
+                        </div>`;
+            //ezaje kardan Item be in tag
+            cartItemsWrapper.append(product);
+        });
+    }
+
+function removeFromCart(id) {
+    let products = $.cookie(cookieName);
+    //length tedad item ha ro dare
+    products = JSON.parse(products);
+    //find kardan oni ke id barabar ba paraneter vordi
+    const itemToRemove = products.findIndex(x => x.id === id);
+    //mige az on be bad ro chanta hazf konam
+    products.splice(itemToRemove, 1);
+    //save dar cookie var zaman expire va masir ke ba path da masir root yane "/" ast
+    //dar baghe Update cookie ast
+    $.cookie(cookieName, JSON.stringify(products), { expires: 2, path: "/" });
+    updateCart();
+
+
 }
