27f0c58dc6b7b0dd92429cf98aa1c0e2bf2adeb1
alipazdar.projects@gmail.com <alipazdar.projects@gmail.com>
2022-08-11 13:39:57 +0430

    برای اضافه کردن آیتم به لیست از طریق چی کوئری کار می کنیم که ابتدا باید آی دی محصول و مشخصاتش رو داشته باشیتم پاس بدیم به چی کوئری <a class="cart-btn" onclick="addToCart('@Model.Product.Id', '@Model.Product.Name', '@Model.Product.DoublePrice', '@Model.Product.Picture')">                                                     <i class="ion-bag"></i> افزودن به سبد خرید   </a> بعد از نوشتن جی کوئری مربوط به سبد خرید که در مسیر  ~/Theme/assets/js/custom.js است باید چند فایل دیگه رو هم به لیوت اضافه کنیم . 1)<script src="~/Theme/assets/js/custom.js"></script> 2)<script src="~/lib/jquery/dist/jquery.min.js"></script> 3)اضافه کردن پلاگین به مسیر Theme/assets/js/ که در واقع یک پلاگین هست برای کار کردن با کوکی <script src="~/Theme/assets/js/plugins.js"></script> که این پلاگین حتما باید قبل از کاستوم باشد. به این شکل نحوع قرارگیری آنها در لیوت     <script src="~/lib/jquery/dist/jquery.min.js"></script>     <script src="~/Theme/assets/js/plugins.js"></script>     <script src="~/Theme/assets/js/custom.js"></script> می شود کد های جی کوئری را تریس کرد با کلمه کلیدی     debugger; و امکانانت موجود در مرورگر

---
+++ LampShade/ServiceHost/wwwroot/Theme/assets/js/custom.js
@@ -0,0 +1,41 @@
+﻿const cookieName = "cart-items";
+
+function addToCart(id, name, price, picture) {
+    debugger;
+
+    //gereftan cookie be name cart-item 
+    let products = $.cookie(cookieName);
+    //emkan dare asasan cart-item az ghabl vojod nadashte bashe age vojod nadash ono mikone ye Arratye empty
+    if (products === undefined) {
+        products = [];
+    } else {
+        //chon cookie az no e string hast tabdil mikonim be json
+        //item pars product ro tabdil mokone be ye Object
+        products = JSON.parse(products);
+    }
+    //oni ke Id barabar ba productCount , valuesho migire 
+    const count = $("#productCount").val();
+    //gereftane id product jari
+    const currentProduct = products.find(x => x.id === id);
+    //on Id az ghabl vojod dasht
+    if (currentProduct !== undefined) {
+        //migim age on kala ro az ghabl to listesh dasht age dobare ezefe kard be listesh ezafe kon
+        products.find(x => x.id === id).count = parseInt(currentProduct.count) + parseInt(count);
+    }
+    //age az ghabl vojod nadash  , migirim vaulue hash ro
+    else {
+        const product = {
+            id,
+            name,
+            unitPrice: price,
+            picture,
+            count
+        }
+        //ezafe kardane entekhabe jadid be list ghabli
+        products.push(product);
+    }
+    //save dar cookie var zaman expire va masir ke ba path da masir root yane "/" ast
+    //dar baghe Update cookie ast
+    $.cookie(cookieName, JSON.stringify(products), { expires: 2, path: "/" });
+  
+}
