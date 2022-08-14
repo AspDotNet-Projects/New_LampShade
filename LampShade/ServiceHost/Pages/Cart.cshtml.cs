34d78c931bec622785a158e0e1df20c79140365a
AspDotNet-Projects <Pazdar_ali@yahoo.com>
2022-08-14 09:35:27 +0430

    ایجاد لیست محصولات سبد خرید CartItems

---
+++ LampShade/ServiceHost/Pages/Cart.cshtml.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Nancy.Json;
+using ShopManagement.Application.Contract.Order;
+
+namespace ServiceHost.Pages
+{
+    public class CartModel : PageModel
+    {
+        public List<CartItem> CartItems;
+        public void OnGet()
+        {
+            //JavaScriptSerializer() jahat tabdil cookie string to onject
+            var serializer = new JavaScriptSerializer();
+            var value = Request.Cookies["cart-items"];
+            CartItems = serializer.Deserialize<List<CartItem>>(value);
+            foreach (var item in CartItems)
+            {
+                item.TotalItemPrice = item.Count * item.UnitPrice;
+            }
+
+
+        }
+    }
+}
---
+++ LampShade/ShopMamagement.Application.Contract/Order/CartItem.cs
@@ -0,0 +1,17 @@
+﻿namespace ShopManagement.Application.Contract.Order
+{
+    public class CartItem
+    {
+        public long Id{ get; set; }
+        public string Name { get; set; }
+        public double UnitPrice { get; set; }
+        public string Picture { get; set; }
+        public int Count { get; set; }
+        public double TotalItemPrice { get; set; }
+
+        public CartItem()
+        {
+            
+        }
+    }
+}
