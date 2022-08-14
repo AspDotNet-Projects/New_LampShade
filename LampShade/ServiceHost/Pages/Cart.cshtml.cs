41cf522a99300dcacfda04ace84c7af1738ddf9b
AspDotNet-Projects <Pazdar_ali@yahoo.com>
2022-08-14 15:23:01 +0430

    آیتم حذف از سبد خرید

--- LampShade/ServiceHost/Pages/Cart.cshtml.cs
+++ LampShade/ServiceHost/Pages/Cart.cshtml.cs
@@ -1,4 +1,9 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Nancy.Json;
 using ShopManagement.Application.Contract.Order;
@@ -8,11 +13,12 @@ namespace ServiceHost.Pages
     public class CartModel : PageModel
     {
         public List<CartItem> CartItems;
+        public const string CookieName = "cart-items";
         public void OnGet()
         {
             //JavaScriptSerializer() jahat tabdil cookie string to onject
             var serializer = new JavaScriptSerializer();
-            var value = Request.Cookies["cart-items"];
+            var value = Request.Cookies[CookieName];
             CartItems = serializer.Deserialize<List<CartItem>>(value);
             foreach (var item in CartItems)
             {
@@ -20,6 +26,19 @@ namespace ServiceHost.Pages
             }
 
 
+        }
+
+        public IActionResult OnGetRemoveFromCart(long id)
+        {
+            var serializer = new JavaScriptSerializer();
+            var value = Request.Cookies[CookieName];
+            Response.Cookies.Delete(CookieName);
+            var cartItems = serializer.Deserialize<List<CartItem>>(value);
+            var itemToRemove = cartItems.FirstOrDefault(x => x.Id == id);
+            cartItems.Remove(itemToRemove);
+            var options = new CookieOptions { Expires = DateTime.Now.AddDays(2),IsEssential = true};
+            Response.Cookies.Append(CookieName, serializer.Serialize(cartItems), options);
+            return RedirectToPage("/Cart");
         }
     }
 }
