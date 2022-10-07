3b841e2b1859e2b60528fc53c396733457fdca18
alipazdar.projects@gmail.com <alipazdar.projects@gmail.com>
2022-10-07 19:41:44 +0330

    API

--- LampShade/BlogManagement.Presentation.Api/Startup.cs
+++ LampShade/BlogManagement.Presentation.Api/Startup.cs
@@ -3,18 +3,11 @@ using BlogManagement.Configuration;
 using CommentManagement.Configuration;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using ServiceHost;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace BlogManagement.Presentation.Api
 {
@@ -45,6 +38,7 @@ namespace BlogManagement.Presentation.Api
             });
         }
 
+        //test
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
