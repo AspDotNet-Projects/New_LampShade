a0358ebe7968090d72d05b55cbddf065a3b9a512
alipazdar.projects@gmail.com <alipazdar.projects@gmail.com>
2022-11-18 13:08:25 +0330

    \گزارش گیری یا reportinhg کار با گزارش گیری chart.js اگه اونو سرچ کنی سایت chartjs.org هست که میتونیم از سمپچل های آ« استافده کننیم #############اضافه کردن chart.js به =پروژه 1)ابتدا  cdnjs.com را وارد کنید و با بالا آ»دن سایت chartjsرا سرج کنید و اولین لینکی که میاد رو copt script tag می کنیم و داخل Adminlayout در انتها اضافه میکنیم <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.0.1/chart.min.js" integrity="sha512-tQYZBKe34uzoeOjY9jr3MX7R/mo7n25vnqbnrkskGr4D6YOoPYSpyafUAzQVjV6xAozAqUFIEFsCO4z8mnVBXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
    cloudflare.com که آدرس این سایت هست که ی فضای ابری هست که میشه ریسورس ها رو اونجا آپلود کرد و به آن رفرنس داد که کاملا برای بحث سئو پیشنهاد میشه و خیلی کاربردی هست برای مثال اکه ما بخوایهم پارامتر های سئو سایت رو اندازه گیری کنیم داخل سایت gtmetrix.com یکی از پارامترهای اندازه گیری برای سئو استفادهاز همین سایت هست . https://cdnjs.cloudflare.com  و cdnjsهم قسمتی از اون که چارت ها هم داخل  اون هست.
    
    بعد از اون برای استفاده از اون جارت باید بگیم که در این فرم اجرا بشه (Index)  که اطلاعات گزارش ها باید قالب js خاص داشته باشند که در واقع دیتای ما هست که باید به این گونه باشه .

---
+++ LampShade/ServiceHost/wwwroot/AdminTheme/assets/js/InitChart.js
@@ -0,0 +1,146 @@
+﻿var ctx = document.getElementById("doughnut").getContext('2d');
+var doughnutChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+        labels: ["Red", "Blue", "Yellow", "Green"],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+var ctx = document.getElementById("barr").getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, -20, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+var ctx = document.getElementById("polarArea").getContext('2d');
+var polarAreaChart = new Chart(ctx, {
+    type: 'polarArea',
+    data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, -20, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+debugger;
+var ctx = document.getElementById("radar").getContext('2d');
+var bubbleAreaChart = new Chart(ctx, {
+    type: 'radar',
+    data: {
+        labels: ["Red", "Blue", , "Green", "Purple", "Orange"],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 10, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 2
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
